c959ed49f7a90beac3ee192f82a5290b435bf7a9
takodayo <jmjx0324@gmail.com>
2024-09-13 22:11:53 +0800

    Update recommender_homepage.py

--- recommender_homepage.py
+++ recommender_homepage.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from types import Description_Based.py, Language_Based.py, Price_Based.py
+from types import Description_Based, Language_Based, Price_Based
 
 st.title("🎮Steam Games Recommender🎮")
 st.title("Find The Game Best Suited For ✨YOU✨ ")
